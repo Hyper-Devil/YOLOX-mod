de4ebb5c6021d38ee61cb9b63bd9105d97beac25
“Hyper-Devil” <tywhd@qq.com>
2022-09-02 11:30:06 +0800

    062 063,C3p3替换HorBlock

--- yolox/models/yolo_pafpn.py
+++ yolox/models/yolo_pafpn.py
@@ -56,7 +56,9 @@ class YOLOPAFPN(nn.Module):
             act=act,
         )
         # self.C3_p3_STR = C3STR(int(2 * in_channels[0] * width), int(in_channels[0] * width))
-        
+        self.C3_p3_HorBlock = Block(int(2 * in_channels[0] * width),0.,1e-6,gnconv,3)
+        self.change_chn = BaseConv(int(2 * in_channels[0] * width), int(in_channels[0] * width), 1, 1)
+
         # bottom-up conv
         self.bu_conv2 = Conv(
             int(in_channels[0] * width), int(in_channels[0] * width), 3, 2, act=act
@@ -83,7 +85,7 @@ class YOLOPAFPN(nn.Module):
             depthwise=depthwise,
             act=act,
         )
-        self.C3_n4_HorBlock = Block(int(2 * in_channels[1] * width),0.,1e-6,gnconv,5)
+        # self.C3_n4_HorBlock = Block(int(2 * in_channels[1] * width),0.,1e-6,gnconv,5)
         # self.C3_n4_STR = C3STR(int(2 * in_channels[1] * width), int(in_channels[2] * width), int(in_channels[2] * width // 16), 1)
 
         # 如果在yolox-s 640 下，012对应 128 256 512
@@ -113,17 +115,19 @@ class YOLOPAFPN(nn.Module):
         fpn_out1 = self.reduce_conv1(f_out0)  # 512->256/16
         f_out1 = self.upsample(fpn_out1)  # 256/8
         f_out1 = torch.cat([f_out1, x2], 1)  # 256->512/8
-        pan_out2 = self.C3_p3(f_out1)  # 512->256/8      
+        # pan_out2 = self.C3_p3(f_out1)  # 512->256/8      
         # pan_out2 = self.C3_p3_STR(f_out1)
-
+        pan_out2 = self.C3_p3_HorBlock(f_out1)
+        pan_out2 = self.change_chn(pan_out2)
+        
         p_out1 = self.bu_conv2(pan_out2)  # 256->256/16
         p_out1 = torch.cat([p_out1, fpn_out1], 1)  # 256->512/16
         pan_out1 = self.C3_n3(p_out1)  # 512->512/16
 
         p_out0 = self.bu_conv1(pan_out1)  # 512->512/32
         p_out0 = torch.cat([p_out0, fpn_out0], 1)  # 512->1024/32
-        # pan_out0 = self.C3_n4(p_out0)  # 1024->1024/
-        pan_out0 = self.C3_n4_HorBlock(p_out0)
+        pan_out0 = self.C3_n4(p_out0)  # 1024->1024/
+        # pan_out0 = self.C3_n4_HorBlock(p_out0)
 
         outputs = (pan_out2, pan_out1, pan_out0)
         return outputs
