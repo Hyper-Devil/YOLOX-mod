2208ce60763ae76d6a91205d80caf0fd027fb349
Hyper-Devil <tywhd@qq.com>
2022-09-08 21:41:24 +0800

    106-109,ECA+HorBlock

---
+++ auto_train3.py
@@ -0,0 +1,21 @@
+import pynvml
+import os
+import time
+pynvml.nvmlInit()
+# 这里的0是GPU id
+handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+ratio = 1024**2
+while 1:
+    time.sleep(300)
+    meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    total = meminfo.total/ratio  # 以兆M为单位就需要除以1024**2
+    used = meminfo.used/ratio
+    free = meminfo.free/ratio
+    print("total: ", total)
+    print("used: ", used)
+    print("free: ", free)
+    if used < total/8:
+        print("start")
+        os.system('python -m yolox.tools.train -f exps/example/custom/bit.py -d 1 -b 32 --fp16 --cache -o -expn 108 --logger wandb wandb-project YOLOX wandb-name 108-bit+3ECA+HorBlock')
+        print("finish")
+        break
